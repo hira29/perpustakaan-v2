809ef506dcfc27d84cf6cf609c8f86b373c455ee
hira29 <dzakydio@gmail.com>
2020-05-09 13:24:21 +0700

    add component and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, Inject} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Perpustakaan';
+
+  constructor(@Inject(DOCUMENT) document) { }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+    if (window.pageYOffset > 60) {
+      const element = document.getElementById('navbar');
+      element.classList.add('sticky-header');
+      const element2 = document.getElementById('content');
+      element2.classList.add('content-when-sticky-header');
+    } else {
+      const element = document.getElementById('navbar');
+      element.classList.remove('sticky-header');
+      const element2 = document.getElementById('content');
+      element2.classList.remove('content-when-sticky-header');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
